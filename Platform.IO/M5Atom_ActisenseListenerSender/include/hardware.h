413b0edb7c81c60b410b306b0b680943318ee669
free-x <oroitburd@gmail.com>
2023-07-18 15:28:22 +0200

    Adjust GPIO in M5Atom_ActisenseListenerSender

--- Platform.IO/M5Atom_ActisenseListenerSender/include/hardware.h
+++ Platform.IO/M5Atom_ActisenseListenerSender/include/hardware.h
@@ -1,9 +1,14 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
-#define ESP32_CAN_TX_PIN GPIO_NUM_26  // Set CAN TX port to 26 
-#define ESP32_CAN_RX_PIN GPIO_NUM_32  // Set CAN RX port to 32
-#define BMP_SDA 25
-#define BMP_SCL 21
+#ifdef BOARD_M5ATOM
+#define ESP32_CAN_TX_PIN GPIO_NUM_22  // Set CAN TX port to 22
+#define ESP32_CAN_RX_PIN GPIO_NUM_19  // Set CAN RX port to 19
+#endif
+
+#ifdef BOARD_M5ATOM_CANUNIT
+#define ESP32_CAN_TX_PIN GPIO_NUM_26
+#define ESP32_CAN_RX_PIN GPIO_NUM_32
+#endif
 
 #endif
