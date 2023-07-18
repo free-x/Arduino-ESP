96616365ce9a21d8d8d2616d7c78a5c8a370de99
free-x <oroitburd@gmail.com>
2023-07-18 15:21:53 +0200

    Adjust GPIO in M5Atom_BMP280_Simple

--- Platform.IO/M5Atom_BMP280_Simple/include/hardware.h
+++ Platform.IO/M5Atom_BMP280_Simple/include/hardware.h
@@ -1,9 +1,10 @@
 #ifndef HARDWARE_H
 #define HARDWARE_H
 
-#define ESP32_CAN_TX_PIN GPIO_NUM_26  // Set CAN TX port to 26 
-#define ESP32_CAN_RX_PIN GPIO_NUM_32  // Set CAN RX port to 32
-#define BMP_SDA 25
-#define BMP_SCL 21
+#ifdef BOARD_M5ATOM
+#define ESP32_CAN_TX_PIN GPIO_NUM_22  // Set CAN TX port to 26 
+#define ESP32_CAN_RX_PIN GPIO_NUM_19  // Set CAN RX port to 32
+#define BMP_SDA 26
+#define BMP_SCL 32
 
 #endif
